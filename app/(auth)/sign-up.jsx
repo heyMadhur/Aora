04e6d07bfb77c75b2415c84043803513bde10f43
heyMadhur <madhurgupta12112002@gmail.com>
2024-06-13 13:34:18 +0530

    LogIn & SignUp Logic Done

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import {createUser} from '../../lib/appwrite'
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -13,7 +14,27 @@ const SignUp = () => {
     password: ''
   })
 
-  const submit = () => { }
+  const submit = async() => {
+    if(!form.username || !form.email || !form.password) {
+      Alert.alert('Error', "Please fill in all the fields")
+    }
+    setIsSubmitting(true)
+    try{
+      const result= await createUser(form.email, form.password, form.username)
+
+      // Set it to Global state using Context
+
+      // Navigate to Home Page
+      router.replace('/home')
+      
+      } catch(error) {
+        Alert.alert("Update", "Reached")
+        Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
+
+  }
 
   const [isSubmitting, setIsSubmitting] = useState(false)
   return (
