31b39fe21cf215d2345b30f79c141ea460913843
heyMadhur <madhurgupta12112002@gmail.com>
2024-06-14 11:33:19 +0530

    Profile Page Done with Logic

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,8 +9,11 @@ import { useState, useEffect } from 'react'
 import { getAllPosts, getLatestPost } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const Home = () => {
+
+  const { user, setUser, setIsLoggedIn } = useGlobalContext()
 
   // Rename Data as Posts
   const { data: posts, refetch } = useAppwrite(getAllPosts)
@@ -38,7 +41,7 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
-                <Text className="text-2xl font-psemibold text-white">Madhur Sir</Text>
+                <Text className="text-2xl font-psemibold text-white">{user?.username}</Text>
               </View>
               <View className="mt-1.5"><Image source={images.logoSmall} className="2-9 h-10" resizeMode='contain' /></View>
             </View>
--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,14 +1,89 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text, FlatList, Image, RefreshControl, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { icons, images } from '../../constants'
+import SearchInput from '../../components/SearchInput'
+import EmptyState from '../../components/EmptyState'
+import { useState, useEffect } from 'react'
+import { getAllPosts, getLatestPost, getUserPosts, searchPosts, signOut } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
+import { router, useLocalSearchParams } from 'expo-router'
+import { useGlobalContext } from '../../context/GlobalProvider'
+import InfoBox from '../../components/InformationBox'
 
-const profile = () => {
+
+const Profile = () => {
+
+  const { user, setUser, setIsLoggedIn } = useGlobalContext()
+
+  const { query } = useLocalSearchParams()
+  // Rename Data as searchedPosts
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id))
+
+  const logout = async() => { 
+    await signOut()
+    setUser(null)
+    setIsLoggedIn(false)
+
+    // We have replaced the whole route so that user cant go back to previous screen, once he logged out
+    router.replace('/sign-in')
+
+  }
+
+
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard video={item} />
+        )}
+        ListHeaderComponent={() => (
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              className="w-full items-end mb-10"
+              onPress={logout}
+            >
+              <Image source={icons.logout}
+                resizeMode='contain' className="w-6 h-6"
+              />
+            </TouchableOpacity>
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+              <Image source={{ uri: user?.avatar }}
+                className="w-[90%] h-[90%] rounded-lg" resizeMode='cover' />
+            </View>
+            <InfoBox
+              title={user?.username}
+              containerStyle="mt-5"
+              titleStyles="text-lg"
+            />
+            <View className="mt-5 flex-row">
+
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles="mr-10"
+                titleStyles="text-xl"
+              />
+              <InfoBox
+                title="1.2k"
+                subtitle= "Followers"
+                titleStyles="text-xl"
+              />
+            </View>
+          </View>
+        )}
+
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this search query"
+          />
+        )} />
+    </SafeAreaView>
   )
 }
 
-export default profile
-
-const styles = StyleSheet.create({})
+export default Profile
